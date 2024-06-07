24bc934d3a3d342f78727f45091915acc1b44f49
Ninutsa <ninutsa.nadirashvili@gmail.com>
2024-06-07 21:46:55 +0400

    Add many-to-many association between users and stocks. Add functionality to allow users to follow searched stocks. Display stocks that the user follows under the search input. Update navigation links accordingly.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+  
   def self.new_lookup(ticker_symbol)
     Alphavantage.configure do |config|
       config.api_key = Rails.application.credentials.alphavantage_api_key
@@ -6,11 +11,14 @@ class Stock < ApplicationRecord
     
     begin
       client = Alphavantage::TimeSeries.new(symbol: ticker_symbol).quote
-      company = Alphavantage::Fundamental.new(symbol: 'TSLA').overview
+      company = Alphavantage::Fundamental.new(symbol: ticker_symbol).overview
       new(ticker: ticker_symbol, name: company.name, last_price: client.open)
     rescue => exception
       return nil
     end  
+  end
 
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 end
